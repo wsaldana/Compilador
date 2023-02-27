eb5f8eb87ff78bcd6fdb9016215c65cabf540b3a
wsaldana <sal19897@uvg.edu.gt>
2023-02-26 22:06:20 -0600

    remove unued code

--- src/main.py
+++ src/main.py
@@ -1,15 +1,5 @@
-from afn import AFN
-
-
 def run():
-    thompson = AFN()
-    regex = input("Please enter a regular expression: ")
-    s = input("Please enter a string to match: ")
-
-    while s != "q":
-        print(thompson.match(regex, s))
-        regex = input("\n(q - quit) Please enter a regular expression to match: ")
-        s = input("\n(q - quit) Please enter a string to match: ")
+    pass
 
 
 if __name__ == "__main__":
